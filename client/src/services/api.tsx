aeff19c9769e72bbdc685da7e4ad7fd0257b3868
Christian Mundal <70058199+Mondz16@users.noreply.github.com>
2026-03-08 21:16:28 +0800

    Fixed baseURL

--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskify-0sgg.onrender.com/api",
+  baseURL: "https://taskify-0sgg.onrender.com",
   withCredentials: false,
 });
 
